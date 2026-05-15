2df6dad6bd37a2f22ba40135d1a232f700134925
Xlt-0603 <17717963350@163.com>
2026-05-15 12:59:31 +0800

    防御塔

---
+++ Project_Legions/Assets/scripts/Units/Tower.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace PPCorps
+{
+    public class Tower : UnitBase
+    {
+        public override void OnBeat(int bar, int beat)
+        {
+            if (_isDead || data == null) return;
+
+            _currentTarget = FindNearestEnemy();
+
+            if (_currentTarget != null && InAttackRange(_currentTarget))
+            {
+                TryAttack(_currentTarget);
+            }
+            else
+            {
+                _currentAction = UnitAction.Idle;
+            }
+        }
+    }
+}
